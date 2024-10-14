ad86b9be832e7e3ab5ebca9a92ef77fd60292b50
AoSam40 <ersoy.nazli@student.hs-rm.de>
2024-10-15 00:23:10 +0200

    empty template

--- app/src/main/java/com/example/prodch/MainActivity.java
+++ app/src/main/java/com/example/prodch/MainActivity.java
@@ -1,26 +1,35 @@
 package com.example.prodch;
 
+
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.util.Log;
 import com.example.prodch.databinding.ActivityMainBinding;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.drawerlayout.widget.DrawerLayout;
-import android.os.Bundle;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
+
+
+public class MainActivity extends AppCompatActivity  {
 
 private ActivityMainBinding binding;
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+    public ItemTouchHelper itemtouch;
+    private MenuItem item;
 
-
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("myTag","Start");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -28,14 +37,26 @@ private ActivityMainBinding binding;
         // drawer and back button to close drawer
         drawerLayout = findViewById(R.id.my_drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
         
         // pass the Open and Close toggle for the drawer layout listener
         // to toggle the button
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
+
+
+
+
+
+
+
+
+
+        //menu.addListener();
+
         // to make the Navigation drawer icon always appear on the action bar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
 
      /*binding = ActivityMainBinding.inflate(getLayoutInflater());
@@ -50,12 +71,18 @@ private ActivityMainBinding binding;
      * A native method that is implemented by the 'prodch' native library,
      * which is packaged with this application.
      */
+
+
+
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    public boolean onOptionsItemSelected(final @NonNull MenuItem item) {
+        //list selection
 
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            Log.i("myTag","Drawer");
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
     public native String stringFromJNI();
@@ -64,4 +91,6 @@ private ActivityMainBinding binding;
     static {
         System.loadLibrary("prodch");
     }
+
+
 }
---
+++ app/src/main/java/com/example/prodch/NavMenuRecyclerViewAdapter.java
@@ -0,0 +1,35 @@
+/* com.example.prodch;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NavMenuRecyclerViewAdapter extends RecyclerView.Adapter<NavMenuRecyclerViewAdapter.MyViewHolder> {
+    @NonNull
+    @Override
+    public NavMenuRecyclerViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NavMenuRecyclerViewAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NavMenuRecyclerViewAdapter.MyView holder, int position) {
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NavMenuRecyclerViewAdapter holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+*/
